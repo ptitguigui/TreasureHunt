e11ef42e197047caf2dddbfc5cd9b07f5d5e3447
Leprêtre Guillaume <lespammerdu62@hotmail.fr>
2016-03-14 18:39:26 +0100

    mis a jour du projet

--- treasure hunt/src/Jouer.java
+++ treasure hunt/src/Jouer.java
@@ -6,12 +6,12 @@
 public class Jouer {
 
 	public static void main(String[] args) {
-		String[] imgs={"images/mer.png",
-				"images/rocher.png.jpg",
-				"images/1.navire.png",
-				"images/2.navire.png",
+		String[] imgs={"treasure hunt/images/mer.png",
+				"treasure hunt/images/rocher.png.jpg",
+				"treasure hunt/images/1.navire.png",
+				"treasure hunt/images/2.navire.png",
 				// image clef à trouver
-				"images/coffre.png.jpg"};
+				"treasure hunt/images/coffre.png.jpg"};
 		
 		int taille=10;
 		SuperPlateau grille=new SuperPlateau(imgs,taille);
