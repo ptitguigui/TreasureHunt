d7e755b1c3ea3f8f71d66b27dc35adebd8e9a0a8
vitsem <fenel-chan@hotmail.fr>
2016-03-24 16:54:43 +0100

    Essai de correction de bug

--- treasure hunt/src/Jouer.java
+++ treasure hunt/src/Jouer.java
@@ -17,32 +17,41 @@ public class Jouer {
 				"treasure hunt/images/coffre.png.jpg"};
 		
 		
-		//demande de la taille
 		JOptionPane j=new JOptionPane();
+		//demande de la taille
+			//nombre de colonnes
 		String rep=new String(j.showInputDialog(null,"Entrer la taille du plateau (entre 10 et 20) :"));
-		//Tant que le pourcentage soit un chiffre et qu'il soit entre 10 et 20
+		//Tant que la saisie soit un chiffre et qu'il soit entre 10 et 20
 		while(!(rep.matches("[1-9][0-9]*")&& Integer.parseInt(rep)>=10 && Integer.parseInt(rep)<=20)){
 				j.showMessageDialog(null, "Veuillez entrer un nombre correct.", "Erreur", 0);
 				rep=j.showInputDialog(null,"Entrer la taille du plateau (entre 10 et 20) :");
 		}
-		int taille=Integer.parseInt(rep);
+		int nbColonnes=Integer.parseInt(rep);
+			//nombre de lignes
+		rep=new String(j.showInputDialog(null,"Entrer la taille du plateau (entre 10 et 20) :"));
+		//Tant que la saisie soit un chiffre et qu'il soit entre 10 et 20
+		while(!(rep.matches("[1-9][0-9]*")&& Integer.parseInt(rep)>=10 && Integer.parseInt(rep)<=20)){
+				j.showMessageDialog(null, "Veuillez entrer un nombre correct.", "Erreur", 0);
+				rep=j.showInputDialog(null,"Entrer la taille du plateau (entre 10 et 20) :");
+		}
+		int nbLignes=Integer.parseInt(rep);
 		
 		
 		//demande du pourcentage de rocher
 		rep=new String(j.showInputDialog(null,"Entrer le pourcentage de rocher :"));
-		//Tant que le pourcentage soit un chiffre et que le nombre de rochers soit >=3 et que'il soit <20% 
+		//Tant que la saisie soit un chiffre et que le nombre de rochers soit >=3 et que'il soit <20% 
 		while(!(rep.matches("[1-9][0-9]*")   
-				&& (int)((taille-2)*(taille-2)*Integer.parseInt(rep)/100.00)>=3 
-				&& (int)((taille-2)*(taille-2)*Integer.parseInt(rep)/100.00)<(taille-2)*(taille-2)*0.20-2)){
+				&& (int)((nbColonnes-2)*(nbLignes-2)*Integer.parseInt(rep)/100.00)>=3 
+				&& (int)((nbColonnes-2)*(nbLignes-2)*Integer.parseInt(rep)/100.00)<(nbColonnes-2)*(nbLignes-2)*0.20-2)){
 				j.showMessageDialog(null, "Nombre incorrecte ou pourcentage impossible à réaliser.", "Erreur", 0);
 				rep=j.showInputDialog(null,"Entrer le pourcentage de rocher :");
 		}
 		int pourcentage=Integer.parseInt(rep);
 		
 		//Création de l'ile
-		SuperPlateau grille=new SuperPlateau(imgs,taille);
+		SuperPlateau grille=new SuperPlateau(imgs,nbColonnes);
 		
-		Ile monIle = new Ile(taille,taille);
+		Ile monIle = new Ile(nbColonnes,nbLignes);
 		monIle.initialiser(pourcentage);
 		grille.setJeu(monIle.getIleTab());
 		
