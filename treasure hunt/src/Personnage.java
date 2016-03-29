522e7fabd108b54287acd771014adc485460e465
Guillaume.Lepretre <http.proxy>
2016-03-29 13:37:42 +0200

    conflits

--- treasure hunt/src/Personnage.java
+++ treasure hunt/src/Personnage.java
@@ -12,12 +12,12 @@ public class Personnage {
 		this.energie = energie;
 		this.equipe = equipe;
 	}
-	
+	/*
 	void actionJ1(InputEvent event){
 		if (event instanceof MouseEvent) {
 			 int x = jeuJ1b.getX((MouseEvent) event) ;
 			 int y = jeuJ1b.getY((MouseEvent) event) ;
 			 [equipe].println("ligne " + x + " colonne : " + y ) ;			 
 		 }
-	}
+	}*/
 }
