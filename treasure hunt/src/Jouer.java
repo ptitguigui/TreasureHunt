7d0e2db59ca6768bdf9762bc272739dc3541323e
Guillaume.Lepretre <http.proxy>
2016-03-29 13:23:07 +0200

    conflit

--- treasure hunt/src/Jouer.java
+++ treasure hunt/src/Jouer.java
@@ -66,5 +66,4 @@ public class Jouer {
 		//affichage texte
 		//System.out.println(monIle);
 	}
-
 }
--- treasure hunt/src/Personnage.java
+++ treasure hunt/src/Personnage.java
@@ -1,14 +1,23 @@
+import java.awt.event.InputEvent;
+import java.awt.event.MouseEvent;
 
 public class Personnage {
 	String nom;
 	int energie;
+	int equipe;
 	
-	Personnage(String nom, int energie){
+	
+	Personnage(String nom, int equipe, int energie){
 		this.nom=nom;
 		this.energie = energie;
+		this.equipe = equipe;
 	}
 	
-	void action(){
-		
+	void action(InputEvent event){
+		 if (event instanceof MouseEvent) {
+			 int x = grille[equipe].getX((MouseEvent) event) ;
+			 int y = grille[equipe].getY((MouseEvent) event) ;
+			 grille[equipe].println("ligne " + x + " colonne : " + y ) ;			 
+		 }
 	}
 }
