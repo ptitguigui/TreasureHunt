cd082df4e47b449e4ee5020e0c2ecd34bef55c50
Leprêtre Guillaume <lespammerdu62@hotmail.fr>
2016-04-24 21:29:28 +0200

    conflits

--- treasure hunt/src/ParcelleArbre.java
+++ treasure hunt/src/ParcelleArbre.java
@@ -1,14 +1,5 @@
-/**
- * Classe créant une parcelle correspondant à un arbre.
- * @author vitsem
- *
- */
-<<<<<<< HEAD
-public class ParcelleArbre extends Parcelle {
 
-=======
 public class ParcelleArbre extends Parcelle {
->>>>>>> branch 'master' of https://github.com/ptitguigui/TreasureHunt.git
 
 	/**
 	 * Constructeur initialisant l'arbre.
